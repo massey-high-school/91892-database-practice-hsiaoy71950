7aa9f62cfd93a0bed51c8430d36bc5cbd4434677
hsiaoy71950 <51727091+hsiaoy71950@users.noreply.github.com>
2020-09-01 11:38:46 +1200

    Create add_success.php

---
+++ site_template/add_success.php
@@ -0,0 +1,11 @@
+<?php include("topbit.php") ?>
+
+        <div class="box main">
+            <h2>Congratulations!!!</h2>
+            <p>You have added a game to the database</p>
+            
+
+            
+        </div> <!-- / main -->
+        
+<?php include("bottombit.php") ?>
